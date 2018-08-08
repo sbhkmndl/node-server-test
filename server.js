e1402e9f871715cf4f52a0e99b56f61ac89cb550
sbhkmndl <soubhikmandal007@gmail.com>
2018-08-09 01:37:02 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err)=>{if(err) console.log('cannot connect to the server');});
+  next();
+});
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('toUpper', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.get('/', (req,res)=>{
+  res.render('home.hbs', {
+    messase: 'welcome to our app'
+  });
+});
+
+app.get('/bad', (req, res)=>{
+  res.send({
+    errorMessage: 'page not found',
+    statusCode: 404,
+
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log('Server Started on 3000 port');
+});
